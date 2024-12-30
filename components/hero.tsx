e9d2a1547bbca0acb18a268cbaa28a4fa5c4fc26
syed <syed@kemuri.in>
2024-12-30 23:42:50 +0530

    added links to the home page

--- components/hero.tsx
+++ components/hero.tsx
@@ -8,12 +8,13 @@ const Hero = () => (
           <span className="relative">
             Easiest way to
             <span className="absolute inset-x-0 bottom-0 h-0.5 bg-gradient-to-r from-green-500 via-blue-500 to-purple-500"></span>
-          </span>
-          {" "}Making 💰 Profits{" "}         
-            One Click  {" "}
+          </span>{" "}
+          Making 💰 Profits One Click{" "}
           <span className="relative">
             CopyTrading 🚀
-            <span className="absolute inset-x-0 bottom-0 h-0.5 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500"> </span>
+            <span className="absolute inset-x-0 bottom-0 h-0.5 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
+              {" "}
+            </span>
           </span>
         </h1>
         <br></br>
@@ -25,19 +26,19 @@ const Hero = () => (
         </p>
         <br></br>
         <div className="flex items-center justify-center gap-x-3 font-medium text-sm">
-        <NavLink
-            href="7"
+          <NavLink
+            href="/https://i.bybit.com/269r1abS?action=inviteToCopy"
             className="text-white text-bold text-lg bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 mb-5 hover:from-yellow-500 hover:via-red-500 hover:to-pink-500 ring-offset-2 transition rounded-full"
-        >
-          Start Now (One Click)
-        </NavLink>
-          
-        <NavLink
-          href=""
-          className="text-white text-bold text-lg bg-none border-8 p-2  mb-5  border-none"
-        >
-          View Goals
-        </NavLink>
+          >
+            Start Now (One Click)
+          </NavLink>
+
+          <NavLink
+            href="/challenge"
+            className="text-white text-bold text-lg bg-none border-8 p-2  mb-5  border-none"
+          >
+            View Goals
+          </NavLink>
         </div>
       </div>
     </div>
